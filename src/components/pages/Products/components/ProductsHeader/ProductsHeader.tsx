c5ba2119cea2d19a057e6499b5f8cf1398731d71
Teodor Radulovic <radulovicteodor@gmail.com>
2025-07-29 22:02:39 +0200

    feat: Implement conditional rendering of "Add New Product" button based on user permissions
    - Added permission checks to display the "Add New Product" button only for users with PRODUCT_WRITE permission, enhancing access control in the ProductsOverviewHeader component.
    - Integrated current user state management to determine permission levels, improving user experience and functionality.

--- src/components/pages/Products/components/ProductsHeader/ProductsHeader.tsx
+++ src/components/pages/Products/components/ProductsHeader/ProductsHeader.tsx
@@ -5,14 +5,26 @@ import { Button } from 'flowbite-react'
 import { HiPlus } from 'react-icons/hi'
 import { URLPartsEnum } from '@/shared/enums'
 import ProductsSearch from './ProductsSearch'
+import { useCurrentUserStore } from '@/store/store'
+import { hasPermission } from '@/shared/helpers/permissions'
+import { PermissionEnum } from '@/shared/types'
 
 export const ProductsOverviewHeader = () => {
+  const { currentUser } = useCurrentUserStore()
+
+  const hasWritePermission = hasPermission(
+    currentUser?.permissions,
+    PermissionEnum.PRODUCT_WRITE,
+    currentUser?.role
+  )
   return (
     <div className="w-full flex justify-between items-center flex-wrap gap-4">
       <ProductsSearch />
-      <Button href={URLPartsEnum.AddNewProduct}>
-        Add new <HiPlus className="ml-2" />
-      </Button>
+      {hasWritePermission && (
+        <Button href={URLPartsEnum.AddNewProduct}>
+          Add new <HiPlus className="ml-2" />
+        </Button>
+      )}
     </div>
   )
 }
