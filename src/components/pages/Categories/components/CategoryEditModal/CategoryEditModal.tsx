630dd466cb0948242a98f30387fe8b953dbc2ea7
Teodor Radulovic <radulovicteodor@gmail.com>
2026-01-19 13:36:25 +0100

    fix: Enable save button regardless of category name presence in CategoryEditModal

--- src/components/pages/Categories/components/CategoryEditModal/CategoryEditModal.tsx
+++ src/components/pages/Categories/components/CategoryEditModal/CategoryEditModal.tsx
@@ -281,7 +281,6 @@ const CategoryEditModal: React.FC<ModalType> = ({
             <Button
               onClick={saveFunction}
               className="w-full sm:w-auto order-2 sm:order-1"
-              disabled={!categoryDataRef.current.name}
               color="purple"
             >
               {initialValue ? 'Update Category' : 'Create Category'}
